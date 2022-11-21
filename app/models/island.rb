21ed322ac2c81371fdb9b696661dce77ee92088b
Andrew Birch <birch.fish@gmail.com>
2022-11-21 17:21:56 +0000

    added photo column to island table.  added validations to model.

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,2 +1,7 @@
 class Island < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
+  validates :photo, presence: true
+  belongs_to :user
 end
