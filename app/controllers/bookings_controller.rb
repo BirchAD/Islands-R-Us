e8adecd4f02ceedf80c671963bae0a1f15a93b9c
Andrew Birch <birch.fish@gmail.com>
2022-11-23 12:34:59 +0000

    Basic booking new/create linked to a specific island and a user.  Calculates total price based on price per night

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,35 @@
+class BookingsController < ApplicationController
+  before_action :set_island, only: %i[new create]
+  before_action :set_user, only: %i[new create]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.island = @island
+    @booking.price_per_night = @island.price
+    @booking.total_price = @booking.price_per_night * (@booking.end_date - @booking.start_date)
+    if @booking.save!
+      redirect_to island_path(@island), notice: 'Your booking was successfully created.'
+    else
+      render :new, status: :unprocessable_entity, notice: 'The booking was not created.'
+    end
+  end
+
+  private
+
+  def set_island
+    @island = Island.find(params[:island_id])
+  end
+
+  def set_user
+    @island.user = current_user
+  end
+
+  def booking_params
+    params.require(:booking).permit(:island_id, :start_date, :end_date)
+  end
+end
